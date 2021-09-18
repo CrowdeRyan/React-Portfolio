b959efcc7bcdd8e07d43206de963b44971e313ae
Ryan Crowder <crowderyan@gmail.com>
2021-09-18 09:21:43 -0400

    setting main page

---
+++ src/Landing.js
@@ -0,0 +1,3 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
