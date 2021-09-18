4f91db2e00111afe0d4bbbbd7e6cbe9869433925
Ryan Crowder <crowderyan@gmail.com>
2021-09-18 09:30:38 -0400

    return rendering

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -7,4 +7,71 @@ class Sidebar extends Component {
 
     this.sidebarData = props.sidebarData;
   }
+
+  render() {
+    return (
+      <nav
+        className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top"
+        id="sideNav"
+      >
+        <a className="navbar-brand js-scroll-trigger" href="#page-top">
+          <span className="d-block d-lg-none">
+            {this.sidebarData.firstName} {this.sidebarData.lastName}
+          </span>
+          <span className="d-none d-lg-block">
+            <img
+              className="img-fluid img-profile rounded-circle mx-auto mb-2"
+              src={image}
+              alt=""
+            ></img>
+          </span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#experience">
+                Experience
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#education">
+                Education
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#skills">
+                Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#aboutME">
+                About Me
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#projects">
+                Projects
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
