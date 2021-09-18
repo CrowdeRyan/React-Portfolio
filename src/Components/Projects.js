3548270a9bf942f2ce98c74e1379a3252bbb1a23
Ryan Crowder <crowderyan@gmail.com>
2021-09-18 10:16:31 -0400

    set state

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,3 +1,11 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAward } from "@fortawesome/free-solid-svg-icons";
+
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+
+    this.projects = props.projects;
+  }
+}
