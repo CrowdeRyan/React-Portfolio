dbcf2ebcdf95aeddb68d7b8a8a400520be22f55b
Ryan Crowder <crowderyan@gmail.com>
2021-09-18 09:28:55 -0400

    aligning props

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,2 +1,10 @@
 import React, { Component } from "react";
 import image from "../Assets/headshot.jpg";
+
+class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.sidebarData = props.sidebarData;
+  }
+}
