7dfa3b782c90fbd385554b02461c922cfd815cc6
Ryan Crowder <crowderyan@gmail.com>
2021-09-18 09:22:48 -0400

    aligning props

--- src/Landing.js
+++ src/Landing.js
@@ -1,3 +1,11 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+class Landing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.landingData = props.landingData;
+  }
+}
