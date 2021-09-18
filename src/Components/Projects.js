034fc440ab7e2eb05afa5b061f130e347656ae4e
Ryan Crowder <crowderyan@gmail.com>
2021-09-18 10:17:02 -0400

    build out page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -8,4 +8,25 @@ class Projects extends Component {
 
     this.projects = props.projects;
   }
+
+  render() {
+    return (
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="projects"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Projects &amp; Git Repositories</h2>
+          <ul className="fa-ul mb-0">
+            {this.projects.map((data, index) => (
+              <li key={index}>
+                <FontAwesomeIcon icon={faAward} color="#ffc107" />
+                <span className="ml-2"> {data.projectsDetail} </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    );
+  }
 }
