fa67bb33c623cd52352edac7863d4eddcdcbc9aa
Ryan Crowder <crowderyan@gmail.com>
2021-09-18 09:40:27 -0400

    extend props

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,3 +1,11 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+class Skills extends Component {
+  constructor(props) {
+    super(props);
+
+    this.skills = props.skills;
+  }
+}
