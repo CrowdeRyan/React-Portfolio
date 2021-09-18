33450fff3e9d29402d5b7acc7e900b36e1acdc86
Ryan Crowder <crowderyan@gmail.com>
2021-09-18 09:40:51 -0400

    page view

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -8,4 +8,30 @@ class Skills extends Component {
 
     this.skills = props.skills;
   }
+
+  render() {
+    return (
+      <section
+        className="resume-section p-3 p-lg-5 d-flex align-items-center"
+        id="skills"
+      >
+        <div className="w-100">
+          <h2 className="mb-5">Skills</h2>
+          <div className="subheading mb-3">
+            Programming Languages &amp; Tools
+          </div>
+          <div className="row">
+            {this.skills.map((data, index) => (
+              <div key={index} className="col-6">
+                <p className="list-item">
+                  <FontAwesomeIcon icon={faCheckCircle} color="green" />
+                  <span className="ml-3">{data.name}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
